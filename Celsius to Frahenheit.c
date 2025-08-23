f80602356686418e11aced84f153f9f8a6cc4143
Samridhi-goyal <samridhi.md696@gmail.com>
2025-08-23 15:48:18 +0530

    Add files via upload

---
+++ Celsius to Frahenheit.c
@@ -0,0 +1,13 @@
+//Q5: Write a program to convert temperature from Celsius to Fahrenheit.
+
+#include <stdio.h>
+
+int main()
+{
+  int f,c;
+  printf("Enter the temperature in celsius \n");
+  scanf("%d",&c);
+  f=(c*9/5)+32;
+  printf("The Temperature in fahrenheit is %d" ,f);
+    return 0;
+}
---
+++ Swapping of two numbers.c
@@ -0,0 +1,18 @@
+//Q6: Write a program to swap two numbers using a third variable.
+
+
+#include <stdio.h>
+
+int main()
+{
+  int a,b,c;
+  printf("Enter the first number \n");
+  scanf("%d",&a);
+  printf("Enter the second number \n");
+  scanf("%d",&b);
+  c=a;
+  a=b;
+  b=c;
+  printf("The number a and b after swapping respectively are  %d and %d" ,a,b);
+    return 0;
+}
